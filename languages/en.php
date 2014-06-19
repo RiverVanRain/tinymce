7c608c7ec1ac6e935b6603813fbf74dce4a4fb5d
RiverVanRain <river.vanrain@gmail.com>
2014-06-20 02:54:41 +0300

    Update en.php

--- languages/en.php
+++ languages/en.php
@@ -5,14 +5,11 @@
  * @package ElggTinyMCE
  */
 
-return array(
+$english = array(
 	'tinymce:remove' => "Remove editor",
 	'tinymce:add' => "Add editor",
 	'tinymce:word_count' => 'Word count: ',
 	'tinymce:lang_notice' => "Your site language is %s but it isn't installed for TinyMCE. Get it <a target=\"_blank\" href=\"%s\">here</a> and copy it to %s. Then, <a href=\"%s\">flush the caches</a>. See the TinyMCE README for more details.",
 );
-<<<<<<< HEAD
 
 add_translation("en", $english);
-=======
->>>>>>> b8bb7d7117bfddd96d1d575750a13ec53329468e
